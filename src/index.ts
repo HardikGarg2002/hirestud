4d53b6f5986ad44cea104b84764c7d2e0f88dc9c
Hardik Garg <hardikgarg3085@gmail.com>
2024-04-03 23:36:27 +0530

    error handler middleware added

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,49 @@
-import express from "express";
-import env from "dotenv";
-import mongoose from "mongoose";
+import express from 'express';
+import env from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { ConfigurationError, SystemError, ErrorHandlerMiddleware } from './common/middleware/error-middleware.js';
+import studentRouter from './route/student-route.js';
 
 env.config();
 const port = process.env.PORT || 3000;
-const environment = process.env.ENVIRONMENT || "local";
-const service = process.env.SERVICE || "local";
-const mongoUri = process.env.mongoUri;
-if (!mongoUri || mongoUri == undefined) {
-  throw new Error("mongoUri is not set in the envionment");
+const environment = process.env.ENVIRONMENT || 'local';
+const service = process.env.SERVICE || 'local';
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI || MONGO_URI == undefined) {
+	throw new ConfigurationError('MONGO_URI is not set in the envionment');
 }
-const app = express();
+const app = express().use(cors()).use(express.json());
 
 async function connectToDB(mongoUri: string) {
-  try {
-    await mongoose.connect(mongoUri, {
-      dbName: "hirestud",
-    });
-  } catch (err) {
-    console.log("error in connecting to mongoDB", err);
-  }
+	try {
+		await mongoose.connect(mongoUri, {
+			dbName: 'hirestud',
+		});
+    console.log('database connected successfully')
+	} catch (err) {
+		console.log('error in connecting to mongoDB', err);
+	}
 }
-connectToDB(mongoUri);
+ connectToDB(MONGO_URI);
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Service running",
-    dbConnected: mongoose.connection.readyState === 1,
-  });
+app.get('/', (req, res) => {
+	res.send({
+		message: 'Service running',
+		dbConnected: mongoose.connection.readyState === 1,
+	});
 });
-app.use("*", (req, res) => {
-  res.status(404).send({
-    error: "AUTH404: Could not find the page requested by you",
-  });
-});
+console.log('before student router')
+app.use('/student', studentRouter);
+console.log('after student router')
 
+app.use('*', (req, res) => {
+	res.status(404).send({
+		error: 'AUTH404: Could not find the page requested by you',
+	});
+});
+app.use(ErrorHandlerMiddleware);
 app.listen(port, () => {
-  console.log("App listening on port service is ", port, service);
+	console.log('App listening on port service is ', port, service);
 });
+// https://www.youtube.com/watch?v=T5t46vuW8fo&ab_channel=CodeBucks
